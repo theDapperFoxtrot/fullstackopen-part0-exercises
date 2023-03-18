2bc6e7d08a68d5589b23a50f61aa4728d251481b
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-03-18 13:22:44 +0200

    refactoring data into object

--- Part 1/fullstackopen-part1/src/components/Total.jsx
+++ Part 1/fullstackopen-part1/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = (props) => {
 	return (
 		<>
-			<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+			<p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
 		</>
 	);
 };
