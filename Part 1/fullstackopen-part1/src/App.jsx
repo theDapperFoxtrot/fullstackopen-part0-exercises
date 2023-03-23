a25e4dbdcac69a25e96f2dfd795327afce1096b1
theDapperFoxtrot <60763515+theDapperFoxtrot@users.noreply.github.com>
2023-03-23 10:30:54 +0200

    commenting out unnecessary code

--- Part 1/fullstackopen-part1/src/App.jsx
+++ Part 1/fullstackopen-part1/src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Content from './components/Content';
-import Total from './components/Total';
-import Button from './components/Button';
-import Display from './components/Display';
+// import { useState } from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+// import Button from "./components/Button";
+// import Display from "./components/Display";
 
 const App = () => {
-	const [counter, setCounter] = useState(0);
+	// const [counter, setCounter] = useState(0);
 
-	const increment = () => setCounter(counter + 1);
-	const decrement = () => setCounter(counter - 1);
-	const reset = () => setCounter(0);
+	// const increment = () => setCounter(counter + 1);
+	// const decrement = () => setCounter(counter - 1);
+	// const reset = () => setCounter(0);
 
 	const course = {
-		name: 'Half Stack application development',
+		name: "Half Stack application development",
 		parts: [
 			{
-				name: 'Fundamentals of React',
+				name: "Fundamentals of React",
 				exercises: 10,
 			},
 			{
-				name: 'Using props to pass data',
+				name: "Using props to pass data",
 				exercises: 7,
 			},
 			{
-				name: 'State of a component',
+				name: "State of a component",
 				exercises: 14,
 			},
 		],
@@ -35,10 +35,12 @@ const App = () => {
 			<Header course={course} />
 			<Content course={course} />
 			<Total course={course} />
-			<Display counter={counter} />
-			<Button handleClick={decrement} text='-' />
-			<Button handleClick={reset} text='Reset' />
-			<Button handleClick={increment} text='+' />
+			{/* This was just for fun */}
+			{/* <Display counter={counter} />
+			<Button handleClick={decrement} text="-" />
+			<Button handleClick={reset} text="Reset" />
+			<Button handleClick={increment} text="+" /> */}
+			{/* This was just for fun */}
 		</>
 	);
 };
